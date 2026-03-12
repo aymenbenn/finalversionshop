f1ff361f1b24b18a5b3b8e4706ba3bfebc0fb23e
aymenbenn <aymen.benhassine@esen.tn>
2026-03-12 05:49:22 +0100

    yes

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { motion } from 'framer-motion'
@@ -255,4 +254,4 @@ export function HomePage() {
     </div>
   )
 }
-```
+
